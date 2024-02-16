e3f14f0133bd87175d1b59bf7628926cf9ffc420
fled-xircus <156864572+fled-xircus@users.noreply.github.com>
2024-02-16 22:02:46 +0800

    eslint error updates

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -10,8 +10,8 @@ const AboutSection = () => (
         <Heading as="h2" fontSize={{ base: '2xl', md: '4xl' }} mb={4}>About Me</Heading>
         <Text fontSize={{ base: 'xl', md: '2xl' }} mb={5}>Hi! I am Fled Andree Aguelo</Text>
         <Text fontSize={{ base: 'md', md: 'xl' }}>
-          I'm a skilled computer engineer with a passion for web design. With expertise in front-end development,
-          I create visually appealing, user-friendly websites that meet client needs and industry standards. I'm committed to continuous
+          I am a skilled computer engineer with a passion for web design. With expertise in front-end development,
+          I create visually appealing, user-friendly websites that meet client needs and industry standards. I am committed to continuous
           learning and innovation, staying updated with the latest web technologies to deliver exceptional digital experiences with
           precision and professionalism.
         </Text>
